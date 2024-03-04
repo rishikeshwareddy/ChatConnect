fe7424208100d1db43b145ea0534a19b9b42e80d
rishikeshwareddy <rishikeshwareddyj2003@gmail.com>
2024-03-04 15:16:08 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import connecttoMOngoDB from "./db/connectmongodb.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -10,7 +11,7 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -18,8 +19,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Hello hi");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "frontend/public")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 })
 
 server.listen(PORT, () => {
